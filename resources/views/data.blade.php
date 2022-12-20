8caefd783ee315c7a3f9a90d9a16a6eaff279f0e
geanutami <utamigeanissa@gmail.com>
2022-12-20 17:41:36 +0700

    Judul soal

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -95,7 +95,7 @@
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title">FORM INPUT DATA SOAL </h4>
+                        <h4 class="modal-title">FORM INPUT DATA </h4>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
